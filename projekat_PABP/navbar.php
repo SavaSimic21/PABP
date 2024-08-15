3183560be08b954bc1708977f6fb84f18eac696f
SavaSimic21 <149540597+SavaSimic21@users.noreply.github.com>
2024-08-15 14:29:25 +0200

    Add files via upload

---
+++ projekat_PABP/navbar.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-primary bg-primary">
+    <a class="navbar-brand" href="index.php" style="color: black;">FILMIX</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color: black;">Početna</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="movies.php" style="color: black;">Filmovi</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="series.php" style="color: black;">Serije</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php" style="color: black;">Odjava</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="settings.php" style="color: black;">Podesavanja</a>
+            </li>
+        </ul>
+    </div>
+</nav>
