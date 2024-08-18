ae826142e55d974bfe348d46297d0de5d2f8fbc6
SavaSimic21 <149540597+SavaSimic21@users.noreply.github.com>
2024-08-18 23:56:51 +0200

    Update navbar.php

--- projekat_PABP/navbar.php
+++ projekat_PABP/navbar.php
@@ -1,8 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-primary bg-primary">
-    <a class="navbar-brand" href="index.php" style="color: black;">FILMIX</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+    <a class="navbar-brand" href="index.php" style="color: black;">FILMIX</a>    
     <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item">
